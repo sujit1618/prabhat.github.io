b23d81ab275346937fb0fc6822d0a28f5d5c608f
Sujit <sjoshi1@saic.edu>
2022-11-08 01:51:58 +0530

    c

--- NodeJSTutorial/app.js
+++ NodeJSTutorial/app.js
@@ -27,3 +27,44 @@ app.get('/about',(req,res) => {
 
 //listen on port 3000
 app.listen(port,() => console.info('Listening on port '+port));
+
+//razorpay instance
+var instance = new Razorpay({
+    key_id: 'rzp_test_rZxLZHEKkJ7BWZ',
+    key_secret: 'af8ajaYQLBkRqq9up3UxCtTt',
+  });
+
+//razorpay order create
+const Razorpay = require('razorpay');
+var instance = new Razorpay({ key_id: 'rzp_test_rZxLZHEKkJ7BWZ', key_secret: 'af8ajaYQLBkRqq9up3UxCtTt' })
+
+var options = {
+  amount: req.body.amount,  // amount in the smallest currency unit
+  currency: "INR",
+  receipt: "order_rcptid_11"
+};
+instance.orders.create(options, function(err, order) {
+  console.log(order);
+  res.send({orderId:order.id});
+});
+
+//verifying order signatures
+app.post("/api/payment/verify",(req,res)=>{
+
+    let body=req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
+   
+     var crypto = require("crypto");
+     var expectedSignature = crypto.createHmac('sha256', 'af8ajaYQLBkRqq9up3UxCtTt')
+                                     .update(body.toString())
+                                     .digest('hex');
+                                     console.log("sig received " ,req.body.response.razorpay_signature);
+                                     console.log("sig generated " ,expectedSignature);
+     var response = {"signatureIsValid":"false"}
+     if(expectedSignature === req.body.response.razorpay_signature)
+      response={"signatureIsValid":"true"}
+         res.send(response);
+     });
+   
+   app.listen(port, () => {
+     console.log(`Example app listening at http://localhost:${port}`)
+   })
